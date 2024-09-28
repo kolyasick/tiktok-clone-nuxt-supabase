00a7b61fcac276751217cc07f8e2edb98fb9f63a
kolyasick <igrokfaceit21@gmail.com>
2024-09-28 10:38:51 +0300

    user fixes

---
+++ prisma/migrations/20240928073751_/migration.sql
@@ -0,0 +1,14 @@
+-- DropForeignKey
+ALTER TABLE "Comments" DROP CONSTRAINT "Comments_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comments" DROP CONSTRAINT "Comments_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Videos" DROP CONSTRAINT "Videos_userId_fkey";
